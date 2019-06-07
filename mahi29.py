275926b831ed1688e2713b58763225f9b722d8c3
mahineymar <42170082+mahineymar@users.noreply.github.com>
2019-06-07 14:44:29 +0530

    Update and rename mahi28.py to mahi29.py

--- mahi29.py
+++ mahi29.py
@@ -8,12 +8,12 @@ for i in item:
 for i in range(0,num-1):
   b=0
   for j in range(i+1,num):
-    if x1[i]==x1[j]:
+    if a1[i]==a1[j]:
       b=b+1
       break
-  if b>=1 and x1[i] not in x2:
-    x2.append(x1[i])
-c=len(x2)
+  if b>=1 and a1[i] not in x2:
+    a2.append(a1[i])
+c=len(a2)
 if c==0:
   print("unique")
 else:
